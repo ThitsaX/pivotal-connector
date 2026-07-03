0d55846baa1b5aa2242ac98e2b4ff72dd6a8e2b9
sandi <102009066+sandisoee@users.noreply.github.com>
2026-07-03 17:08:44 +0630

    add apache2.0 license

--- implementation/mod_fspiop_interface/src/main/java/com/thitsaworks/mojaloop/coreconnector/fspiop/model/ErrorInformationObject.java
+++ implementation/mod_fspiop_interface/src/main/java/com/thitsaworks/mojaloop/coreconnector/fspiop/model/ErrorInformationObject.java
@@ -1,4 +1,19 @@
 /*
+ * Copyright (c) 2026 ThitsaWorks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
  * Open API for FSP Interoperability (FSPIOP)
  * Revision date: 2023-11-23 Based on [API Definition updated on 2020-05-19 Version 1.1](https://github.com/mojaloop/mojaloop-specification/blob/main/documents/v1.1-document-set/API%20Definition_v1.1.pdf). This is implementation friendly version of the API definition. It includes the below definitions needed for third-party functionality. - AuthenticationType   - U2F enum - AuthenticationValue   - oneOf is changed to anyOf   - new element is added U2FPinValue - New element U2FPIN **Note:** The API supports a maximum size of 65536 bytes (64 Kilobytes) in the HTTP header.
  *
